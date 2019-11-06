86ca2d5557b021b38a49974cb4181d971f4116b5
林奇 <993643197@qq.com>
2019-11-06 16:02:18 +0800

    项目立项之初的整体架构设计

---
+++ src/main/java/cn/weblade/ccpe/CcpeApplication.java
@@ -0,0 +1,13 @@
+package cn.weblade.ccpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcpeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcpeApplication.class, args);
+    }
+
+}
