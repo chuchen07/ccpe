bf0b568ef778b23d6082abd6e6c7f92600f31869
txx03 <txx19960804@126.com>
2019-12-06 16:41:02 +0800

    完善了课程管理模块

--- src/main/resources/static/js/manage.js
+++ src/main/resources/static/js/manage.js
@@ -138,51 +138,7 @@ function ajaxGetData(currentPage) {
                     .appendTo("#users_table tbody");
             })
 
-            //用户信息管理结束
-            // var video = result.list;
-            // $.each(video,function(index,item){
-            //     var userId =$("<td></td>").append("<input type='text' disabled='ture' value="+item.userId+">");
-            //     var userName = $("<td></td>").append("<input type='text'  value="+item.userName+">");
-            //     var email = $("<td></td>").append("<input type='text' disabled='ture' value="+item.email+">");
-            //     var userCode = $("<td></td>").append("<input type='text'  value="+item.userCode+">");
-            //     var userState = $("<td></td>").append("<input type='text'  value="+item.userState+">");
-            //
-            //     var v_deleBtn=$("<button ></button>").addClass("btn btn-danger ")
-            //         .append($("<span></span>").addClass("glyphicon glyphicon-trash")).append("删除");
-            //
-            //
-            //     // onclick='javascript:this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);'
-            //
-            //     //删除按钮
-            //     v_deleBtn.attr("email",item.email);
-            //     v_deleBtn.click(function () {
-            //
-            //         $.ajax({
-            //
-            //             url:"/deleteUser?time="+new Date().getTime(),//解决数据缓存
-            //             data:"email="+v_deleBtn.attr("email"),
-            //             type:"get",
-            //             success:function(result){
-            //                 if(result==true){
-            //                     alert("成功：该用户已删除");
-            //                     ajaxGetData();
-            //                 }else{
-            //                     alert("失败：删除用户失败");
-            //                 }
-            //             }
-            //         });
-            //     });
-            //
-            //     var btn = $("<td></td>").append(" ").append(v_deleBtn)
-            //
-            //     $("<tr></tr>").append(userId)
-            //         .append(userName)
-            //         .append(email)
-            //         .append(userCode)
-            //         .append(userState)
-            //         .append(btn)
-            //         .appendTo("#video_table tbody");
-            // })
+
     }
 })
 
--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,35 +1,27 @@
 ajaxGetData();
 var cacheData= goTo();//读取缓存的对象
 function ajaxGetData(currentPage) {
-    $("#test_table tbody").empty();
-    var currentPage=currentPage || 1;
+
     $.ajax({
-        url:"/data",
-        data:{currentPage:currentPage,pageAmount:5},
+        url:"/queryAllPaperName",
         type:"get",
         success:function(result){
-
-
-
 
             //用户信息管理结束
             var test = result.list;
             $.each(test,function(index,item3){
-                var email = $("<td></td>").append(item3.email);
+                var paper_Name = $("<td></td>").append(item3.paper_Name);
                 var t_deleBtn=$("<button ></button>").addClass("btn btn-danger ")
                     .append($("<span></span>").addClass("glyphicon glyphicon-trash")).append("删除");
 
-
-                // onclick='javascript:this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);'
-
                 //删除按钮
-                t_deleBtn.attr("email",item3.email);
+                t_deleBtn.attr("paper_Name",item3.paper_Name);
                 t_deleBtn.click(function () {
 
                     $.ajax({
 
-                        url:"/deleteUser?time="+new Date().getTime(),//解决数据缓存
-                        data:"email="+t_deleBtn.attr("email"),
+                        url:"/deleteSubject?time="+new Date().getTime(),//解决数据缓存
+                        data:"paper_Name="+t_deleBtn.attr("paperName"),
                         type:"get",
                         success:function(result){
                             if(result==true){
@@ -45,16 +37,12 @@ function ajaxGetData(currentPage) {
                 var btn = $("<td></td>").append(" ").append(t_deleBtn)
 
                 $("<tr></tr>")
-                    .append(email)
-
+                    .append(paper_Name)
                     .append(btn)
                     .appendTo("#test_table tbody");
             })
         }
     })
-
-
-//以上用户信息管理分页结束
 
 
     function goTo() {
