5cd5ac2d830d2d0849be35f3b553dea15809609e
杨锦聪 <1079734065@qq.com>
2019-12-12 00:37:02 +0800

    增加分页

--- src/main/java/cn/weblade/ccpe/service/Videoservice.java
+++ src/main/java/cn/weblade/ccpe/service/Videoservice.java
@@ -4,12 +4,13 @@ import cn.weblade.ccpe.entity.CourseContext;
 import cn.weblade.ccpe.entity.Video;
 
 import java.util.List;
+import java.util.Map;
 
 public interface Videoservice {
     public List<CourseContext> getcourse();
     public int addVideo(Video video);
     public int addVideo2(Integer videoId, String courseName);
     public int deletevideo(Integer videoId);
-    public List<Video> getvideo(Video video);
+    public List<Video> getvideo(Map map);
     public List<Video> test();
 }
--- src/main/resources/static/js/video.js
+++ src/main/resources/static/js/video.js
@@ -1,41 +1,41 @@
-Data();
+/*Data();
 //var cacheData= goTo();//读取缓存的对象
 function Data() {
     $("#video_table tbody").empty();
 
     $.ajax({
-        url:"/queryVideoByCourseAndName",
-        type:"post",
-        success:function(result){
+        url: "/queryVideoByCourseAndName",
+        type: "post",
+        success: function (result) {
 
-            $.each(result,function(index,item2){
+            $.each(result, function (index, item2) {
 
                 var videoName = $("<td></td>").append(item2.videoName);
                 var courseName = $("<td></td>").append(item2.courseName);
 
 
-                var v_deleBtn=$("<button ></button>").addClass("btn btn-danger ")
+                var v_deleBtn = $("<button ></button>").addClass("btn btn-danger ")
                     .append($("<span></span>").addClass("glyphicon glyphicon-trash")).append("删除");
 
 
                 // onclick='javascript:this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);'
 
                 //删除按钮
-                v_deleBtn.attr("delete",item2.email);
+                v_deleBtn.attr("delete", item2.email);
                 v_deleBtn.click(function () {
 
                     $.ajax({
 
-                        url:"/deletevideo",
-                        data:{
+                        url: "/deletevideo",
+                        data: {
                             videoId: item2.videoId
                         },
-                        type:"post",
-                        success:function(result){
-                            if(result.deletevideo==1){
+                        type: "post",
+                        success: function (result) {
+                            if (result.deletevideo == 1) {
                                 alert("成功：该视频已删除");
                                 Data();
-                            }else{
+                            } else {
                                 alert("失败：删除视频失败");
                             }
                         }
@@ -47,30 +47,83 @@ function Data() {
                 $("<tr></tr>")
                     .append(videoName)
                     .append(courseName)
-
                     .append(btn)
                     .appendTo("#video_table tbody");
             })
         }
     })
+}*/
 
+function showvideolist(start,size) {
+    $("#video_table tbody").empty();
+    $.ajax({
+            url: "/queryVideoByCourseAndName",
+            type: "post",
+            data:{
+                start: start,
+                size: size,
+            },
+        success:function (videolist) {
+            $.each(videolist,function (index,video) {
+                var videoName = $("<td></td>").append(video.videoName);
+                var courseName = $("<td></td>").append(video.courseName);
+                var v_deleBtn = $("<button ></button>").addClass("btn btn-danger ")
+                    .append($("<span></span>").addClass("glyphicon glyphicon-trash")).append("删除");
+                v_deleBtn.click(function () {
 
-//以上用户信息管理分页结束
+                    $.ajax({
 
+                        url: "/deletevideo",
+                        data: {
+                            videoId: video.videoId
+                        },
+                        type: "post",
+                        success: function (result) {
+                            if (result.deletevideo == 1) {
+                                alert("成功：该视频已删除");
+                                showvideolist(0,5);
+                            } else {
+                                alert("失败：删除视频失败");
+                            }
+                        }
+                    });
+                });
 
-    // function goTo() {
-    //     var cache = {};//闭包缓存池
-    //     return{
-    //         set:function(currentPage,data){
-    //             cache[currentPage] =data;
-    //         },//添加方法
-    //         get:function (currentPage) {
-    //             if (currentPage in cache){
-    //                 console.log("数据已缓存，无需再请求");
-    //             }else {
-    //                 Data();
-    //             }
-    //         }//读取方法
-    //     }
-    // }
+                var btn = $("<td></td>").append(" ").append(v_deleBtn);
+                $("<tr></tr>")
+                    .append(videoName)
+                    .append(courseName)
+
+                    .append(btn)
+                    .appendTo("#video_table tbody");
+
+            });
+        }
+        }
+    )
 }
+
+showvideolist(0,5);
+layui.use(['laypage','layer'],function () {
+    var laypage = layui.laypage, layer = layui.layer;
+    laypage.render(
+        {
+            elem: 'pagen',
+            count: 50,
+            theme: '#FF5722',
+            limit: 5,
+            curr: 1,
+            prev: '上一页',
+            next: '下一页',
+            jump: function (obj,first) {
+                if(!first){
+                    showvideolist(obj.curr*obj.limit - obj.limit,obj.limit);
+                }
+
+            }
+        }
+    );
+    
+});
+
+
